a28859ace4608f3d91a13744fd87dcf5ac720615
Michał Łazowik <m.lazowik@student.uw.edu.pl>
2015-06-07 14:10:17 +0200

    Dodana obsługa wielu słowników

--- src/dictionary/dictionary.c
+++ src/dictionary/dictionary.c
@@ -16,8 +16,14 @@
 #include "conf.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <argz.h>
 
 #define _GNU_SOURCE
+
+#define conc(str1,str2) #str1 "/" #str2
 
 /**
   Struktura przechowująca słownik.
@@ -38,6 +44,25 @@ struct dictionary
 static void dictionary_free(struct dictionary *dict)
 {
     trie_done(dict->trie);
+}
+
+/*
+ Zwraca, czy plik jest obecnym lub nadrzędnym katalogiem.
+ */
+static bool file_is_current_or_parent_dir(char *filename)
+{
+    return (strcmp(filename, ".") == 0 || strcmp(filename, "..") == 0);
+}
+
+static FILE * open_dict_file(const char *lang, char *permissions)
+{
+    char path[strlen(CONF_PATH) + strlen(lang) + 1];
+
+    strcpy(path, CONF_PATH);
+    strcat(path, "/");
+    strcat(path, lang);
+
+    return fopen(path, permissions);
 }
 
 /**@}*/
@@ -121,6 +146,61 @@ void dictionary_hints(const struct dictionary *dict, const wchar_t* word,
     trie_done(hints);
 
     word_list_sort(list);
+}
+
+int dictionary_lang_list(char **list, size_t *list_len)
+{
+    *list = NULL;
+    *list_len = 0;
+
+    DIR *dir = opendir(CONF_PATH);
+
+    if (dir == NULL) return -1;
+
+    struct dirent *dirent;
+
+    while ((dirent = readdir(dir)) != NULL) {
+        if (!file_is_current_or_parent_dir(dirent->d_name)
+            && argz_add(list, list_len, dirent->d_name) != 0)
+        {
+            free(*list);
+            *list = NULL;
+            *list_len = 0;
+            return -1;
+        }
+    }
+
+    closedir(dir);
+
+    return 0;
+}
+
+struct dictionary * dictionary_load_lang(const char *lang)
+{
+    FILE *dict_file;
+
+    if (!(dict_file = open_dict_file(lang, "r+"))) return NULL;
+
+    struct dictionary *dict = dictionary_load(dict_file);
+
+    fclose(dict_file);
+
+    return dict;
+}
+
+int dictionary_save_lang(const struct dictionary *dict, const char *lang)
+{
+    mkdir(CONF_PATH, 0700);
+
+    FILE *dict_file;
+
+    if (!(dict_file = open_dict_file(lang, "w+"))) return -1;
+
+    int ret = dictionary_save(dict, dict_file);
+
+    fclose(dict_file);
+
+    return ret;
 }
 
 /**@}*/
