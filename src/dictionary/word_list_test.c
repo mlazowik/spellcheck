94447bc4360b497acf8b2b4b6014cd6ac16e6e66
Michał Łazowik <m.lazowik@student.uw.edu.pl>
2015-06-08 13:58:56 +0200

    Zastosowanie konwencji klamer

--- src/dictionary/word_list_test.c
+++ src/dictionary/word_list_test.c
@@ -10,14 +10,16 @@ const wchar_t* first  = L"First string";
 const wchar_t* second = L"Second string";
 const wchar_t* third  = L"Third string";
 
-static void word_list_init_test(void** state) {
+static void word_list_init_test(void** state)
+{
     struct word_list l;
     word_list_init(&l);
     assert_int_equal(word_list_size(&l), 0);
     word_list_done(&l);
 }
 
-static void word_list_add_test(void** state) {
+static void word_list_add_test(void** state)
+{
     struct word_list l;
     word_list_init(&l);
     word_list_add(&l, test);
@@ -26,10 +28,10 @@ static void word_list_add_test(void** state) {
     word_list_done(&l);
 }
 
-
-static int word_list_setup(void **state) {
+static int word_list_setup(void **state)
+{
     struct word_list *l = malloc(sizeof(struct word_list));
-    if (!l) 
+    if (!l)
         return -1;
     word_list_init(l);
     word_list_add(l, first);
@@ -38,30 +40,35 @@ static int word_list_setup(void **state) {
     *state = l;
     return 0;
 }
-static int word_list_teardown(void **state) {
+
+static int word_list_teardown(void **state)
+{
     struct word_list *l = *state;
     word_list_done(l);
     free(l);
     return 0;
 }
 
-static void word_list_get_test(void** state) {
+static void word_list_get_test(void** state)
+{
     struct word_list *l = *state;
     assert_true(wcscmp(first, word_list_get(l)[0]) == 0);
     assert_true(wcscmp(second, word_list_get(l)[1]) == 0);
     assert_true(wcscmp(third, word_list_get(l)[2]) == 0);
 }
 
-static void word_list_repeat_test(void** state) {
+static void word_list_repeat_test(void** state)
+{
     struct word_list *l = *state;
     word_list_add(l, third);
     assert_int_equal(word_list_size(l), 4);
     assert_true(wcscmp(third, word_list_get(l)[3]) == 0);
 }
 
-
-int main(void) {
-    const struct CMUnitTest tests[] = {
+int main(void)
+{
+    const struct CMUnitTest tests[] =
+    {
         cmocka_unit_test(word_list_init_test),
         cmocka_unit_test(word_list_add_test),
         cmocka_unit_test_setup_teardown(word_list_get_test, word_list_setup, word_list_teardown),
